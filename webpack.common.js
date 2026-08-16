08d74de34b982c2b38b15f7048f39b6ab9b185e6
DaviLimS <davilimasoares1@outlook.com>
2026-08-16 19:00:11 -0300

    I made the css start to work and difined the grid

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,10 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 export default {
     entry: {
