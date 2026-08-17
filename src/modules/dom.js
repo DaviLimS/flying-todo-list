26d9cb8918c2b3b7fc6f7d83f34a4dc408a1508c
DaviLimS <davilimasoares1@outlook.com>
2026-08-17 09:28:09 -0300

    I started to create the card system

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,5 +1,21 @@
 import { template } from "./storage.js";
 
-export function loadScreen() {
+const cardConstructor = (info) => {
+    const cardTemplate = document.createElement('div');
+    cardTemplate.classList.add('card');
+    const title = document.createElement('h2');
+    //Fazer checkbox depois
+    const description = document.createElement('p');
+    //fazer o field project dps
 
+    title.textContent = info.title || '';
+    description.textContent = info.description || '';
+
+    cardTemplate.appendChild(title, description);
+}
+const main = document.querySelector('.main');
+
+export function loadScreen() {
+    let defCard = cardConstructor(template);
+    main.append(defCard);
 }
