515d1763bd9bee6d34e9a642225aaadbd4e03c9d
DaviLimS <davilimasoares1@outlook.com>
2026-08-17 10:01:00 -0300

    I started to improve the card layout

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,6 +3,14 @@ import { template } from "./storage.js";
 const cardConstructor = (info) => {
     const cardTemplate = document.createElement('div');
     cardTemplate.classList.add('card');
+
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.classList.add('headerCard')
+    const middleDiv = document.createElement('div');
+    middleDiv.classList.add('middleCard');
+    const lowerDiv = document.createElement('div');
+    lowerDiv.classList.add('lowCard');
+
     const title = document.createElement('h2');
     title.classList.add('cardTitle');
     //Fazer checkbox depois
@@ -13,7 +21,11 @@ const cardConstructor = (info) => {
     title.textContent = info.title || '';
     description.textContent = info.description || '';
 
-    cardTemplate.append(title, description);
+    headerDiv.appendChild(title);
+    middleDiv.appendChild(description);
+    //lowerDiv.appendChild();
+
+    cardTemplate.append(headerDiv, middleDiv, lowerDiv);
     return cardTemplate;
 }
 const main = document.querySelector('.main');
